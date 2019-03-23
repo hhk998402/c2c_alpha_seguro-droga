f89077cf7277011a2384a04acf06232a13003827
hhk998402 <hemant.hari.kumar@gmail.com>
2019-03-23 15:03:54 +0530

    Admin Home Page Done

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dashboard', { title: 'Express' });
+  res.render('dashboard');
 });
 
 router.get('/signup', function(req, res, next) {
-    res.render('signup', { title: 'Express' });
+    res.render('signup');
+});
+
+router.get('/adminHome', function(req, res, next) {
+    res.render('adminHome');
 });
 
 module.exports = router;
